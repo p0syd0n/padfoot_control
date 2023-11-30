ffd6dae7f13a2fd618dba16913e1e3cd931d8f1d
p0syd0n <ajeff9752@gmail.com>
2023-11-30 15:56:44 +0000

    added some stuff (correct server link, and linux dist)

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ match DEBUG:
     case True:
         SERVER = 'https://3000-p0syd0n-padfootserver-14zsp1owg0h.ws-us104.gitpod.io'
     case False:
-        SERVER = 'https://padfoot-server.adaptable.app'
+        SERVER = 'https://posydon.ddns.net:3000'
 
 API_KEY ="WQNV12Vr4bsLEY8VAMdyhpbp1kTewosS7ARe0H47JEfdQcxwjwBokr9p2Xm34qGhm2G6oesk3fLVYsbBKv5hBb79PKNMyDZL2MNSEkTGuVs0SyoCn4dM0MEknHH8vQF9"# os.environ["API_KEY"]#idc if it is leaked whatever
 LATE_MODULE_OUTPUT = "log"#os.environ["LATE_MODULE_OUTPUT"] #log/info
